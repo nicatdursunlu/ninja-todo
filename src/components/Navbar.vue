f449591486ed9c5843c652698d1bd05ce06cc45e
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-03-24 23:06:53 +0400

    update project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <v-toolbar flat app class="text-uppercase grey--text">
+      <v-toolbar-title>
+        <span>Todo</span>
+        <span class="font-weight-bold">Ninja</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn color="grey" dark>
+        <span>Sign out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
