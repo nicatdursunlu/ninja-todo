4882d1f2206b29b9bbb5f820f2f64d71be51dc0d
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-03-24 23:48:06 +0400

    create pages

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,49 @@
 <template>
   <nav>
-    <v-toolbar flat app class="text-uppercase grey--text">
-      <v-toolbar-title>
+    <v-toolbar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="toggleDrawer"/>
+      <v-toolbar-title class="text-uppercase grey--text">
         <span>Todo</span>
         <span class="font-weight-bold">Ninja</span>
       </v-toolbar-title>
-      <v-spacer />
+      <v-spacer/>
       <v-btn color="grey" dark>
         <span>Sign out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer temporary app v-model="drawer" class="primary">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    drawer: false,
+    links: [
+      {icon: 'dashboard', text: 'Dashboard', route: '/'},
+      {icon: 'folder', text: 'My Projects', route: '/projects'},
+      {icon: 'person', text: 'Team', route: '/team'},
+    ]
+  }),
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer
+    }
+  }
+}
 </script>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Dashboard from "@/views/Dashboard";
+import Projects from "@/views/Projects";
+import Team from "@/views/Team";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/projects',
+        name: 'Projects',
+        component: Projects
+    },
+    {
+        path: '/team',
+        name: 'Team',
+        component: Team
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
