73391baee06e07cf0d20dc39f2e63cdc0bec3c27
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-03-25 18:13:40 +0400

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,13 +7,21 @@
         <span class="font-weight-bold">Ninja</span>
       </v-toolbar-title>
       <v-spacer/>
-      <v-btn color="grey" dark>
+      <v-btn color="grey" dark plain>
         <span>Sign out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
 
-    <v-navigation-drawer app v-model="drawer" class="primary">
+    <v-navigation-drawer temporary app v-model="drawer" class="purple accent-3" width="300">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="https://raw.githubusercontent.com/iamshaunjp/vuetify-playlist/lesson-20/todo-ninja/public/avatar-1.png">
+          </v-avatar>
+          <p class="subtitle-1 white--text mt-1">The Net Ninja</p>
+        </v-flex>
+      </v-layout>
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,14 +2,68 @@
   <div class="team">
     <h1 class="subtitle-2 grey--text">Team</h1>
 
+
     <v-container class="my-10">
-      content
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-center grey lighten-2 ma-3">
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="pink">
+                <img :src="person.avatar">
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subtitle-1 red--text darken-4">{{ person.name }}</div>
+              <div class="orange--text darken-3">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn plain color="purple accent-4">
+                <v-icon left>message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    team: [
+      {
+        name: 'The Net Ninja',
+        role: 'Web developer',
+        avatar: 'https://raw.githubusercontent.com/iamshaunjp/vuetify-playlist/lesson-20/todo-ninja/public/avatar-1.png'
+      },
+      {
+        name: 'Ryu',
+        role: 'Graphic designer',
+        avatar: 'https://raw.githubusercontent.com/iamshaunjp/vuetify-playlist/lesson-20/todo-ninja/public/avatar-2.png'
+      },
+      {
+        name: 'Chun Li',
+        role: 'Web developer',
+        avatar:
+            'https://raw.githubusercontent.com/iamshaunjp/vuetify-playlist/lesson-20/todo-ninja/public/avatar-3.png'
+      },
+      {
+        name: 'Gouken',
+        role: 'Social media maverick',
+        avatar:
+            'https://raw.githubusercontent.com/iamshaunjp/vuetify-playlist/lesson-20/todo-ninja/public/avatar-4.png'
+      },
+      {
+        name: 'Yoshi',
+        role: 'Sales guru',
+        avatar:
+            'https://raw.githubusercontent.com/iamshaunjp/vuetify-playlist/lesson-20/todo-ninja/public/avatar-5.png'
+      }
+    ]
+  })
+}
 </script>
 
 <style scoped>
