39dede2649888dcedd015208ac3038697a90baa7
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-03-25 17:36:26 +0400

    add tooltips

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,7 @@
       </v-btn>
     </v-toolbar>
 
-    <v-navigation-drawer  app v-model="drawer" class="primary">
+    <v-navigation-drawer app v-model="drawer" class="primary">
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,25 +1,47 @@
 <template>
   <div class="dashboard">
     <h1 class="subtitle-2 grey--text">Dashboard</h1>
-
     <v-container class="my-10">
-      <v-card flat class="pa-3">
-        <v-layout row wrap>
+      <v-layout row class="mb-8">
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn small class="grey white--text mr-3" @click="sortBy('title')" slot="activator" v-bind="attrs"
+                   v-on="on">
+              <v-icon left small>folder</v-icon>
+              <span class="caption text-lowercase">By project name</span>
+            </v-btn>
+          </template>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn small class="grey white--text" @click="sortBy('person')" slot="activator" v-bind="attrs" v-on="on">
+              <v-icon left small>person</v-icon>
+              <span class="caption text-lowercase">By person</span>
+            </v-btn>
+          </template>
+          <span>Sort projects by person</span>
+        </v-tooltip>
+      </v-layout>
+
+      <v-card flat class="grey lighten-4" v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-6 project ${project.status}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">Project Title</div>
-            <div>Create new Website</div>
+            <div>{{ project.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>The Net Ninja</div>
+            <div>{{ project.person }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
-            <div class="caption grey--text">Due by </div>
-            <div>1st March 2021</div>
+            <div class="caption grey--text">Due by</div>
+            <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-            <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div class="right">
+              <v-chip small :class="`white--text my-3 caption ${project.status}`">{{ project.status }}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
       </v-card>
@@ -28,9 +50,69 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    projects: [
+      {
+        title: 'Design a new website',
+        person: 'The Net Ninja',
+        due: '1st Jan 2019',
+        status: 'ongoing',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+      },
+      {
+        title: 'Code up the homepage',
+        person: 'Chun Li',
+        due: '10th Jan 2019',
+        status: 'complete',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+      },
+      {
+        title: 'Design video thumbnails',
+        person: 'Ryu',
+        due: '20th Dec 2018',
+        status: 'complete',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+      },
+      {
+        title: 'Create a community forum',
+        person: 'Gouken',
+        due: '20th Oct 2018',
+        status: 'overdue',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+      },
+    ]
+  }),
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
+    }
+  }
+}
 </script>
 
 <style scoped>
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
 
+.project.ongoing {
+  border-left: 4px solid orange
+}
+
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+
+.v-chip.complete {
+  background: #3cd1c2;
+}
+
+.v-chip.ongoing {
+  background: #ffaa2c;
+}
+
+.v-chip.overdue {
+  background: #f83e70;
+}
 </style>
