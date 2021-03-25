e2757c389ac632906933f04344483650df05a3cb
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-03-25 21:03:13 +0400

    add Popup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
   <nav>
+    <!--    toolbar-->
     <v-toolbar flat app>
       <v-app-bar-nav-icon class="grey--text" @click="toggleDrawer"/>
       <v-toolbar-title class="text-uppercase grey--text">
@@ -7,19 +8,46 @@
         <span class="font-weight-bold">Ninja</span>
       </v-toolbar-title>
       <v-spacer/>
+
+      <!--      dropdown menu -->
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              color="grey"
+              dark
+              plain
+              v-bind="attrs"
+              v-on="on"
+          >
+            <v-icon left>expand_more</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn color="grey" dark plain>
         <span>Sign out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
 
+    <!--    drawer menu-->
     <v-navigation-drawer temporary app v-model="drawer" class="purple accent-3" width="300">
       <v-layout column align-center>
         <v-flex class="mt-5">
           <v-avatar size="100">
-            <img src="https://raw.githubusercontent.com/iamshaunjp/vuetify-playlist/lesson-20/todo-ninja/public/avatar-1.png">
+            <img
+                src="https://raw.githubusercontent.com/iamshaunjp/vuetify-playlist/lesson-20/todo-ninja/public/avatar-1.png">
           </v-avatar>
           <p class="subtitle-1 white--text mt-1">The Net Ninja</p>
+        </v-flex>
+        <v-flex class="mt-4 mb-3">
+          <Popup />
         </v-flex>
       </v-layout>
       <v-list>
@@ -37,7 +65,12 @@
 </template>
 
 <script>
+import Popup from "@/components/Popup";
+
 export default {
+  components: {
+    Popup
+  },
   data: () => ({
     drawer: false,
     links: [
---
+++ src/components/Popup.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+          color="green accent-3"
+          dark
+          v-bind="attrs"
+          v-on="on"
+      >
+        Add new project
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <h2>Add a New Project</h2>
+      </v-card-title>
+
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
